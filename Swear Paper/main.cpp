a4765e4de315689aafdf58db1aaa2ec415ec2b92
geekskick <geeks_kick@yahoo.co.uk>
2016-08-27 20:04:42 +0100

    Initial Commit

---
+++ Swear Paper/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Swear Paper
+//
+//  Created by Patrick Mintram on 27/08/2016.
+//  Copyright © 2016 Patrick Mintram. All rights reserved.
+//
+
+#include <iostream>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core/core.hpp>
+						
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "OpenCV Version: " << CV_VERSION << std::endl;
+    return 0;
+}
