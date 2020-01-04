5595383f17211656cac50448f109609dab119e00
geekskick <geeks_kick@yahoo.co.uk>
2020-01-04 16:43:58 +0000

    Removes ansi codes source file

--- src/ansi_codes.hpp
+++ src/ansi_codes.hpp
@@ -1,14 +1,13 @@
 #ifndef ANSI_CODES_H
 #define ANSI_CODES_H
 
-#include <string>
+#include <string_view>
 
-class ansi_codes {
-public:
-    const static std::string red;
-    const static std::string reset;
-    const static std::string yellow;
-    const static std::string green;
+struct ansi_codes {
+    constexpr static std::string_view red {"\u001b[31m"};
+    constexpr static std::string_view reset{"\u001b[0m"};
+    constexpr static std::string_view yellow{"\u001b[33m"};
+    constexpr static std::string_view green{"\u001b[32m"};
 };
 
 #endif
