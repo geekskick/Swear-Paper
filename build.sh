49cdc0097fc5d058c1b35126769a95835ac6cc83
geekskick <geeks_kick@yahoo.co.uk>
2022-09-01 18:35:42 +0100

    Works

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -ex
 docker run \
     --rm \
+    -it \
     --mount type=bind,source=`pwd`,target=`pwd` \
     --mount type=volume,source=gcc-conan,target=/root/.conan \
     -w `pwd` \
