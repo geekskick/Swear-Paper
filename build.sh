2fed8e02dfa4a1dbdb5995d988269929e6c850e5
geekskick <geeks_kick@yahoo.co.uk>
2022-09-02 06:08:34 +0000

    source moved to lib

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@ docker run \
     --rm \
     -it \
     --mount type=bind,source=`pwd`,target=`pwd` \
-    --mount type=volume,source=gcc-conan,target=/root/.conan \
+    --mount type=volume,source=gcc-conan,target=/root/conan \
+    --env CONAN_USER_HOME=/root/conan \
     -w `pwd` \
     swear_paper  \
     "${@}"
